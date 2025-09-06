4fa4dd890fc9c925c9fbbf16009975d476279732
Mehedi Hasan Sadhin <164299003+therealsadhin@users.noreply.github.com>
2025-09-06 17:14:56 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+   preview: {
+    allowedHosts: true
+  },
   plugins: [
     react(),
     mode === 'development' &&
